7c25785d3605e2ce9d0d12ff507ed7dcc91c19c6
sorfeb <112263712+sorfeb@users.noreply.github.com>
2023-09-18 01:25:46 +0700

    POP QUIZ W02-2: Scele Scrap - DONE

--- TXT/WEEK02.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Find strings with 10-digit numbers, then sort, and remove duplicates
-grep -Eo '[0-9]{10}' inputSCRAP.txt | sort | uniq
-
-# Save output to outputSCRAP.txt (append mode)
-ls -l >> outputSCRAP.txt 
-
-exit 0
